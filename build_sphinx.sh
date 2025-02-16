2e6d555c59dd397c76be2d42a5d2244986e3e705
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-02-16 16:48:48 -0500

    updating from micromamba to miniforge3 for the builder dockerfile

--- build_sphinx.sh
+++ build_sphinx.sh
@@ -1,1 +1,2 @@
 docker build . -t rltools/documentation-builder -f Dockerfile_sphinx $@
+docker run -it --rm -v $(pwd):/mount rltools/documentation-builder cp /environment_sphinx.yml /mount
--- run_sphinx.sh
+++ run_sphinx.sh
@@ -1,5 +1,4 @@
 docker run -it --rm --platform linux/amd64 \
---user $(id -u):$(id -g) \
 -v $(pwd):/workspace/\:rw -w /workspace \
 rltools/documentation-builder \
 "$@"
